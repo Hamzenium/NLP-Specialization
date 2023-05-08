e60eb76ab6de3c19cb91c2cc3891c24c270773c5
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-05-08 20:18:57 +0300

    Add all my files

---
+++ logistic_regression.py
@@ -0,0 +1,72 @@
+import math
+import numpy as np
+
+def sigmoid(z):
+    h = 1 / (1 + np.exp(-z))
+    return h
+
+
+
+def gradient_descent(x , y, w, alpha , iterations):
+    m = x.shape[0]
+    for i in range(iterations):
+        z = np.dot(x, w)
+        h = sigmoid(z)
+
+        J = -1./m * (np.dot(y.transpose(), np.log(h)) + np.dot((1-y).transpose(),np.log(1-h)))
+        theta = theta - (alpha/m) * np.dot(x.transpose(),(h-y))
+    return J, theta
+
+
+def extract_features(tweet, freqs):
+    '''
+    Input: 
+        tweet: a list of words for one tweet
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+    Output: 
+        x: a feature vector of dimension (1,3)
+    '''
+    # process_tweet tokenizes, stems, and removes stopwords
+    word_l = process_tweet(tweet)
+    
+    # 3 elements in the form of a 1 x 3 vector
+    x = np.zeros((1, 3)) 
+    
+    #bias term is set to 1
+    x[0,0] = 1 
+    
+    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+    
+    # loop through each word in the list of words
+    for word in word_l:
+        
+        # increment the word count for the positive label 1
+        x[0,1] += freqs.get((word, 1.0),0)
+        
+        # increment the word count for the negative label 0
+        x[0,2] += freqs.get((word, 0.0),0)
+        
+    ### END CODE HERE ###
+    assert(x.shape == (1, 3))
+    return x
+
+def predict_tweet(tweet, freqs, theta):
+    '''
+    Input: 
+        tweet: a string
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+        theta: (3,1) vector of weights
+    Output: 
+        y_pred: the probability of a tweet being positive or negative
+    '''
+    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+    
+    # extract the features of the tweet and store it into x
+    x = extract_features(tweet,freqs)
+    
+    # make the prediction using x and theta
+    y_pred = sigmoid(np.dot(x,theta))
+    
+    ### END CODE HERE ###
+    
+    return y_pred
